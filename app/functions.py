a48dcbbaa37bc70262e6c8ee81c0c3cbfe4262b3
Antonio Rivas <toneterivas@gmail.com>
2021-07-19 23:26:24 +0200

    impl. functions.py

---
+++ app/functions.py
@@ -0,0 +1,46 @@
+import re
+import sys
+import logging
+
+def get_platform():
+    platforms = {
+        'linux1' : 'Linux',
+        'linux2' : 'Linux',
+        'darwin' : 'OS X',
+        'win32' : 'Windows'
+    }
+    if sys.platform not in platforms:
+        return sys.platform
+    
+    return platforms[sys.platform]    
+
+def get_number(s):
+    return re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", s)
+
+
+def setup_logger(name, log_file, level=logging.INFO):
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+
+def check_chapters_are_different(current_chapter, chapter):
+    number_current_chapter = get_number(current_chapter)
+    number_chapter = get_number(chapter)
+    
+    if len(number_chapter) > 0 and len(number_current_chapter) > 0 and number_chapter[0] != current_chapter[0]:
+        if int(number_current_chapter[0]) < int(number_chapter[0]):
+            return True
+        else:
+            return False
+    elif current_chapter != chapter:
+        return True
+    else:
+        return False
